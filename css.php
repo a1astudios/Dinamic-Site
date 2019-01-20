70266dc2367cc07c4c62e657c58886be5edafe66
Giuliano Cavallo <thecavallogallery@yahoo.com>
2019-01-20 12:49:43 +0700

    Add files via upload

--- css.php
+++ css.php
@@ -4,13 +4,16 @@
 
 ?>	
 
-
+<!-- latest compiled and minified CSS -->
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-	
+
+<!-- Optional theme -->
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
-	
+
+<!-- jQuery CSS -->	
 <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
-	
+
+<!-- FontAwesome -->	
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 
 <style>
--- js.php
+++ js.php
@@ -4,8 +4,13 @@
 
 ?>
 
+
+<!-- jQuery -->
 <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-	
+
+<!-- jQuery UI -->	
 <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-	
+
+<!-- Latest compiled and minified Javascript -->	
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/bootstrap.min.js"></script>
+
